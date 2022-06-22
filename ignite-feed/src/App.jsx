a033d4358912affaabe245cf22c8789dff07abd7
lirasusejdev <lis.amaral@grupoboticario.com.br>
2022-06-22 14:54:42 -0300

    add instructure

---
+++ ignite-feed/src/App.jsx
@@ -0,0 +1,14 @@
+import { Header } from './components/Header'
+import { Post } from './components/Post'
+import './styles.css'
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <Post author="Lis Amaral" content="lorem ipsum"></Post>
+      <Post author="Gabriel Martins" content="lorem ipsum"></Post>
+    </div>
+  )
+}
+export default App
