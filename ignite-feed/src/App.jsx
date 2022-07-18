3f504e98576c79fe7bfdc58bece264661b891f00
lirasusejdev <lis.amaral@grupoboticario.com.br>
2022-07-18 18:11:22 -0300

    refactor App

--- ignite-feed/src/App.jsx
+++ ignite-feed/src/App.jsx
@@ -6,6 +6,7 @@ import { Sidebar } from './components/Sidebar'
 import { Navbar } from './components/Navbar'
 import React from 'react'
 import Breadcrumbs, { BreadcrumbsItem } from '@atlaskit/breadcrumbs'
+import { AlertButton } from '@kiwicom/orbit-components/lib/Alert'
 
 export function App() {
   return (
@@ -15,11 +16,21 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
+          <Breadcrumbs>
+            <BreadcrumbsItem
+              href="/item"
+              text="Home"
+              aria-label="breadcrumb"
+              separator=">"
+            />
+            <BreadcrumbsItem href="/item" text="Previous Page" />
+            <BreadcrumbsItem href="/item" text="Current Page" />
+          </Breadcrumbs>
           <Post author="Lis Amaral" content="lorem ipsum"></Post>
           <Post author="Victor Eduardo Oliveira" content="lorem ipsum"></Post>
         </main>
         <Breadcrumbs>
-          <BreadcrumbsItem href="/item" text="Item 1" />
+          <BreadcrumbsItem href="/item" text="Item 1" iconAfter={2} />
           <BreadcrumbsItem href="/item" text="Item 2" />
           <BreadcrumbsItem href="/item" text="Item 3" />
           <BreadcrumbsItem href="/item" text="Item 4" />
@@ -27,6 +38,7 @@ export function App() {
           <BreadcrumbsItem href="/item" text="Item 6" />
           <BreadcrumbsItem href="/item" text="Item 7" />
           <BreadcrumbsItem href="/item" text="Item 8" />
+          <AlertButton>Current Page</AlertButton>
         </Breadcrumbs>
       </div>
     </div>
